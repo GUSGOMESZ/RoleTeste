ea20bdff7f80c94c8cc8cb857545f6d830906afc
PurpleG <oliveira.giu0103@gmail.com>
2025-05-01 17:42:55 -0300

    fix: subject translation logic

--- src/Helpers/translate.js
+++ src/Helpers/translate.js
@@ -1,26 +1,26 @@
 const translate = (sub) => {
     switch (sub) {
-        case "Portuguese":
-            return "Português";
-        case "Literature":
-            return "Literatura";
-        case "History":
-            return "Historia";
-        case "Geography":
-            return "Geografia";
-        case "Philosophy":
-            return "Filosofia";
-        case "Sociology":
-            return "Sociologia";
-        case "Math":
-            return "Matematica";
-        case "Physics":
-            return "Fisica";
-        case "Chemical":
-            return "Quimíca";
-        default:
-            return sub; // Caso padrão para evitar erros
+      case "Portuguese":
+        return "Portugues";
+      case "Literature":
+        return "Literatura";
+      case "History":
+        return "Historia";
+      case "Geography":
+        return "Geografia";
+      case "Philosophy":
+        return "Filosofia";
+      case "Sociology":
+        return "Sociologia";
+      case "Math":
+        return "Matematica";
+      case "Physics":
+        return "Fisica";
+      case "Chemical":
+        return "Quimica";
+      default:
+        return sub;
     }
-};
-
-export default translate;
+  };
+  
+  export default translate;
